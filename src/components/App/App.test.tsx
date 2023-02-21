5e342eace3b6de9e5ca93fec0a10717acae18a23
Anastasiya Rashchynskaya <arashchynskaya@edmunds.com>
2023-02-21 20:45:41 +0100

    task 3: Integration with BE

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -18,6 +18,8 @@ test("Renders products list", async () => {
       description: "Product 1 description",
       price: 1,
       count: 1,
+      url: "test.com",
+      author: "autor",
     },
     {
       id: "2",
@@ -25,6 +27,8 @@ test("Renders products list", async () => {
       description: "Product 2 description",
       price: 2,
       count: 2,
+      url: "test.com",
+      author: "autor",
     },
   ];
   server.use(
